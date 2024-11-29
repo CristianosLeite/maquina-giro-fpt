d5271399dc94c2999ea7ae9c5dbd82eddfa9aa47
Cristiano Leite <cristianosleite@hotmail.com>
2024-11-29 18:34:06 -0300

    Add routes and docs

--- api/controllers/operation.controller.js
+++ api/controllers/operation.controller.js
@@ -3,18 +3,32 @@ const Operation = require("../models/operation.model.js");
 
 class OperationController {
   constructor() {
-    this.router = Router();
+    this.router = Router(); // Cria um objeto de roteamento
     this.router.post("/create", this.create.bind(this));
     this.router.get("/all", this.all.bind(this));
-    this.router.get("/one", this.retrieve.bind(this));
+    this.router.get("/one", this.retrieveById.bind(this));
+    this.router.get("/partnumber", this.retrieveByPartnumber.bind(this));
+    this.router.get("/reg_num", this.retrieveByRegNum.bind(this));
     this.router.put("/update", this.update.bind(this));
     this.router.delete("/delete", this.delete.bind(this));
   }
 
+  /**
+   * Retorna o roteador
+   * @returns { Router }
+   */
   getRouter() {
     return this.router;
   }
 
+  /**
+   * Salva a operação no banco de dados
+   * @param {*} req 
+   * @param {*} res
+   * @param { Operation } req.body
+   * @returns { Operation }
+   * @example /api/operation/create
+   */
   async create(req, res) {
     const newOperation = req.body;
 
@@ -30,13 +44,27 @@ class OperationController {
     });
   }
 
+  /**
+   * Devolve todas as operações cadastradas no banco de dados
+   * @param {*} req 
+   * @param {*} res
+   * @returns { Operation[] }
+   * @example /api/operation/all
+   */
   async all(req, res) {
     await Operation.findAll().then((operations) => {
       res.json(operations);
     });
   }
 
-  async retrieve(req, res) {
+  /**
+   * Retorna a operação pelo id
+   * @param {*} req
+   * @param {*} res
+   * @returns { Operation }
+   * @example /api/operation/one?operation_id=1
+   */
+  async retrieveById(req, res) {
     const id = req.query["operation_id"];
 
     if (!id) res.status(400).send("Missing operation_id");
@@ -46,6 +74,13 @@ class OperationController {
     });
   }
 
+  /**
+   * Retorna a operação pelo desenho do motor
+   * @param {*} req
+   * @param {*} res
+   * @returns { Operation[] }
+   * @example /api/operation/partnumber?partnumber=123456
+   */
   async retrieveByPartnumber(req, res) {
     const partnumber = req.query["partnumber"];
 
@@ -58,6 +93,13 @@ class OperationController {
     );
   }
 
+  /**
+   * Retorna a operação pelo número de matrícula
+   * @param {*} req 
+   * @param {*} res
+   * @returns { Operation[] }
+   * @example /api/operation/reg_num?reg_num=123456
+   */
   async retrieveByRegNum(req, res) {
     const regNum = req.query["reg_num"];
 
@@ -68,6 +110,13 @@ class OperationController {
     });
   }
 
+  /**
+   * Retorna a operação pelo status
+   * @param {*} req 
+   * @param {*} res
+   * @returns { Operation[] }
+   * @example /api/operation/status_ok?status_ok=true
+   */
   async retrieveByStatus(req, res) {
     const status = req.query["status_ok"];
 
@@ -78,10 +127,16 @@ class OperationController {
     });
   }
 
+  /**
+   * Retorna a operação pelo código da receita
+   * @param {*} req 
+   * @param {*} res
+   * @returns { Operation[] }
+   */
   retrieveByEngineModelNum(req, res) {
-    const engineModelNum = req.query["engine_model_num"];
+    const engineModelNum = req.query["recipe"];
 
-    if (!engineModelNum) res.status(400).send("Missing engine_model_num");
+    if (!engineModelNum) res.status(400).send("Missing recipe");
 
     Operation.findAll({ where: { engine_model_num: engineModelNum } }).then(
       (operations) => {
@@ -90,6 +145,11 @@ class OperationController {
     );
   }
 
+  /**
+   * Atualiza uma operação
+   * @param {*} req 
+   * @param {*} res 
+   */
   async update(req, res) {
     const id = req.body["operation_id"];
     const operationToUpdate = req.body;
@@ -109,6 +169,11 @@ class OperationController {
     });
   }
 
+  /**
+   * Deleta uma operação
+   * @param {*} req 
+   * @param {*} res 
+   */
   async delete(req, res) {
     const id = req.query["operation_id"];
 
