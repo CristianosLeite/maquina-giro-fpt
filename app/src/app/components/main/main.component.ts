958d588d82181ecb0531d401aa593a29795fb7aa
Cristiano Leite <cristianosleite@hotmail.com>
2024-12-01 22:46:47 -0300

    start toolbar implementation

--- app/src/app/components/main/main.component.ts
+++ app/src/app/components/main/main.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { TableComponent } from '../table/table.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { ToolbarComponent } from "../toolbar/toolbar.component";
 
 @Component({
   selector: 'app-main',
   imports: [
     TableComponent,
     MatToolbarModule,
-    MatSidenavModule
-  ],
+    MatSidenavModule,
+    ToolbarComponent
+],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
--- app/src/app/components/toolbar/toolbar.component.ts
+++ app/src/app/components/toolbar/toolbar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatIconModule
+  ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
-export class ToolbarComponent {
-
-}
+export class ToolbarComponent {}
